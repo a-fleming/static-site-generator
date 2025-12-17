68f80aaab55edd03a81e3a34e248c72019ffb294
Aaron Fleming <amfleming15@gmail.com>
2025-12-17 02:46:50 -0800

    Add LeafNode and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,18 @@
 class HTMLNode:
     def __init__(self, tag: str=None, value: str=None, children: list=None, props: dict=None):
+        if tag is not None:
+            if type(tag) is not str:
+                raise TypeError("tag must be a string if provided")
+        if value is not None:
+            if type(value) is not str:
+                raise TypeError("value must be a string if provided")
+        if children is not None:
+            if type(children) is not list:
+                raise TypeError("children must be a list if provided")
+        if props is not None:
+            if type(props) is not dict:
+                raise TypeError("props must be a dictionary if provided")
+            
         self.tag = tag
         self.value = value
         self.children = children # if children is not None else []
@@ -16,4 +29,16 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str, value: str, props: dict=None):
+        super().__init__(tag=tag, value=value, children=None, props=props)
+        
+        if value is None:
+            raise ValueError("LeafNode requires a value")
     
+    def to_html(self) -> str:
+        if self.tag is None:
+            return self.value
+        props_html = self.props_to_html()
+        return f'<{self.tag}{props_html}>{self.value}</{self.tag}>'
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -10,6 +10,29 @@ class TestHTMLNode(unittest.TestCase):
         self.assertIsNone(node.value)
         self.assertIsNone(node.children)
         self.assertIsNone(node.props)
+    
+    def test_init_with_values(self):
+        node = HTMLNode(tag="div", value="Hello", children=["child1", "child2"], props={"property": "value"})
+        self.assertEqual(node.tag, "div")
+        self.assertEqual(node.value, "Hello")
+        self.assertEqual(node.children, ["child1", "child2"])
+        self.assertEqual(node.props, {"property": "value"})
+    
+    def test_init_invalid_tag(self):
+        with self.assertRaises(TypeError):
+            node = HTMLNode(tag=123)
+    
+    def test_init_invalid_value(self):
+        with self.assertRaises(TypeError):
+            node = HTMLNode(value=456)
+    
+    def test_init_invalid_children(self):
+        with self.assertRaises(TypeError):
+            node = HTMLNode(children="not a list")
+    
+    def test_init_invalid_props(self):
+        with self.assertRaises(TypeError):
+            node = HTMLNode(props="not a dict")
     
     def test_to_html_not_implemented(self):
         node = HTMLNode()
@@ -34,6 +57,48 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode()
         expected_repr = "HTMLNode(tag=None, value=None, children=None, props=None)"
         self.assertEqual(repr(node), expected_repr)
+
+class TestLeafNode(unittest.TestCase):
+    def test_init_missing_tag(self):
+        with self.assertRaises(TypeError):
+            node = LeafNode(value="Some text")
+
+    def test_init_missing_value(self):
+        with self.assertRaises(TypeError):
+            node = LeafNode(tag="p")
+    
+    def test_init_value_none(self):
+        with self.assertRaises(ValueError):
+            node = LeafNode(tag="p", value=None)
+    
+    def test_init_with_children_keyword(self):
+        with self.assertRaises(TypeError):
+            node = LeafNode(tag="p", value="Some text", children=[])
+        
+        with self.assertRaises(TypeError):
+            node = LeafNode(tag="p", value="Some text", children=["child1", "child2"])   
+    
+    def test_init_with_children_positional(self):
+        with self.assertRaises(TypeError):
+            node = LeafNode("p", "Some text", [], {"prop": "value"})
+        
+        with self.assertRaises(TypeError):
+            node = LeafNode("p", "Some text", ["child1", "child2"], {"prop": "value"})
+    
+    def test_to_html(self):
+        node = LeafNode(tag="span", value="Leaf Node", props={"class": "leaf"})
+        expected_html = '<span class="leaf">Leaf Node</span>'
+        self.assertEqual(node.to_html(), expected_html)
+    
+    def test_to_html_no_props(self):
+        node = LeafNode(tag="div", value="No Props")
+        expected_html = '<div>No Props</div>'
+        self.assertEqual(node.to_html(), expected_html)
+    
+    def test_to_html_raw_text(self):
+        node = LeafNode(tag=None, value="Raw text")
+        expected_html = 'Raw text'
+        self.assertEqual(node.to_html(), expected_html)
 
 
 if __name__ == "__main__":
