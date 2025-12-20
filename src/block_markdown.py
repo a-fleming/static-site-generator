3b5bbc8203f6957de86647f8fe57d68976f0c698
Aaron Fleming <amfleming15@gmail.com>
2025-12-20 02:54:56 -0800

    Add functionality to determine BlockType of a block

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,69 @@
-def markdown_to_blocks(text: str) -> list:
-    if type(text) != str:
-        raise TypeError("Markdown text must be a string.")
-    return [block.strip() for block in text.split("\n\n") if block != ""]
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
+
+def block_to_block_type(block: str) -> BlockType:
+    if is_code_block(block):
+        return BlockType.CODE
+    
+    block_lines = block.split("\n")
+    if is_heading_block(block_lines):
+        return BlockType.HEADING
+    if is_quote_block(block_lines):
+        return BlockType.QUOTE
+    if is_unordered_list_block(block_lines):
+        return BlockType.ULIST
+    if is_ordered_list_block(block_lines):
+        return BlockType.OLIST
+    return BlockType.PARAGRAPH
+
+def is_code_block(block: str) -> bool:
+    if len(block) > 6 and block[:3] == block[-3:] == "```":
+        return True
+    return False
+
+def is_heading_block(block_list: list) -> bool:
+    if len(block_list) != 1:
+        return False
+    words = block_list[0].split(" ")
+    if len(words) < 2:
+        return False
+    lead_chars = words[0]
+    if len(lead_chars) < 1 or len(lead_chars) > 6:
+        return False
+    
+    for i in range(len(lead_chars)):
+        if lead_chars[i] != "#":
+            return False
+    return True
+
+def is_quote_block(block_list: list) -> bool:
+    for line in block_list:
+        if len(line) < 2 or line[:2] != "> ":
+            return False
+    return True
+
+def is_unordered_list_block(block_list: list) -> bool:
+    for line in block_list:
+        if len(line) < 2 or line[:2] != "- ":
+            return False
+    return True
+
+def is_ordered_list_block(block_list: list) -> bool:
+    for number, line in enumerate(block_list, start=1):
+        if len(line) < 3 or line[:3] != f"{number}. ":
+            return False
+    return True
+
+
+def markdown_to_blocks(markdown: str) -> list:
+    if type(markdown) != str:
+        raise TypeError("Markdown must be a string.")
+    return [block.strip() for block in markdown.split("\n\n") if block != ""]
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,8 +1,93 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import (
+    BlockType,
+    block_to_block_type,
+    markdown_to_blocks
+)
 
-class TestInlineMarkdown(unittest.TestCase):
+class TestBlockMarkdown(unittest.TestCase):
+    def test_block_to_block_type_paragraph(self):
+        block = "This is a \bparagraph block\nwith some text."
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_heading(self):
+        block = "# This is a heading."
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_block_to_block_type_heading_h6(self):
+        block = "###### This is an h6 heading."
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_block_to_block_type_code(self):
+        block = "```This is a\ncode block\nwith code```"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.CODE)
+
+    def test_block_to_block_type_quote(self):
+        block = "> This is a\n> quote block\n> with some text"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_block_to_block_type_unordered_list(self):
+        block = "- This is an\n- unordered list\n- with items"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.ULIST)
+
+    def test_block_to_block_type_ordered_list(self):
+        block = "1. This is an\n2. ordered list\n3. with items"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.OLIST)
+
+    def test_block_to_block_type_malformed_heading_missing_space(self):
+        block = "#This is a malformed heading."
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_malformed_heading_extra_octothorpe(self):
+        block = "####### This is a malformed heading."
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_malformed_code_bad_start(self):
+        block = "``This is a\ncode block\nwith code```"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_malformed_code_bad_end(self):
+        block = "``This is a\ncode block\nwith code`"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_malformed_quote_missing_space(self):
+        block = "> This is a\n>malformed\n- quote block"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    # This test might need to be removed when we no longer split blocks by looking for "\n\n"
+    def test_block_to_block_type_malformed_heading_extra_line(self):
+        block = "# This is another\n# malformed heading."
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_malformed_unordered_list_missing_space(self):
+        block = "- This is a\n-malformed\n- unordered list"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_malformed_ordered_list_missing_space(self):
+        block = "1. This is an\n2.improperly ordered\n3. list"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_malformed_ordered_list_not_sequential(self):
+        block = "1. This is an\n4. improperly ordered\n3. list"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
     def test_markdown_to_blocks_single(self):
         md = "This is **bolded** paragraph"
         blocks = markdown_to_blocks(md)
