bc852f6f597ab2a37cd29d1e3794f56ee999dedf
Aaron Fleming <amfleming15@gmail.com>
2025-12-25 01:16:12 -0800

    Configure project for hosting on GitHub Pages

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import shutil
+import sys
 
 from pathlib import Path
 
@@ -6,15 +7,16 @@ from block_markdown import markdown_to_html_node
 from textnode import TextNode, TextType
 
 def main():
+    basepath = sys.argv[1] if len(sys.argv) >= 2 else "/"
     src_dir = "static"
-    dest_dir = "public"
+    dest_dir = "docs" # used for GitHub pages
     copy_directory_contents(src_dir, dest_dir, remove_dest=True, verbose=True)
     
     content_path = "content"
     template_path = "template.html"
-    dest_path = "public"
-    if generate_pages_recursive(content_path, template_path, dest_path, verbose=False):
-        print("Successfully generated page from '{content_path}' to '{dest_path}' using '{template_path}'")
+    dest_path = "docs" # used for GitHub pages
+    if generate_pages_recursive(content_path, template_path, dest_path, basepath, verbose=False):
+        print(f"Successfully generated page from '{content_path}' to '{dest_path}' using '{template_path}'")
     else:
         print(f"Failed to generate page from '{content_path}' to '{dest_path}' using '{template_path}'")
 
@@ -78,7 +80,24 @@ def extract_title(markdown: str):
             return section[2:]
     raise ValueError("Markdown does not contain a header.")
 
-def generate_page(from_path: str | Path, template_path:str | Path, dest_path: str | Path, verbose: bool=False) -> bool:
+def generate_page(from_path: str | Path, template_path:str | Path, dest_path: str | Path, basepath: str | Path, verbose: bool=False) -> bool:
+    if not isinstance(from_path, (str, Path)):
+        print(f"from_path must be a string or Path object.")
+        return False
+    from_path = Path(from_path)
+    if not isinstance(template_path, (str, Path)):
+        print(f"template_path must be a string or Path object.")
+        return False
+    template_path = Path(template_path)
+    if not isinstance(dest_path, (str, Path)):
+        print(f"dest_path must be a string or Path object.")
+        return False
+    dest_path = Path(dest_path)
+    if not isinstance(basepath, (str, Path)):
+        print(f"basepath must be a string or Path object.")
+        return False
+    basepath = Path(basepath)
+
     print(f"Generating page from '{from_path}' to '{dest_path}' using '{template_path}'")
 
     try:
@@ -100,6 +119,8 @@ def generate_page(from_path: str | Path, template_path:str | Path, dest_path: st
 
     template_content = template_content.replace("{{ Title }}", title)
     template_content = template_content.replace("{{ Content }}", html)
+    template_content = template_content.replace('href="/', f'href="{basepath}/')
+    template_content = template_content.replace('src="/', f'src="{basepath}/')
 
     try:
         with open(dest_path, 'w') as dest_file:
@@ -109,7 +130,7 @@ def generate_page(from_path: str | Path, template_path:str | Path, dest_path: st
         return False
     return True
 
-def generate_pages_recursive(content_dir_path: str | Path, template_path: str | Path, dest_dir_path: str | Path, verbose=False) -> bool:
+def generate_pages_recursive(content_dir_path: str | Path, template_path: str | Path, dest_dir_path: str | Path, basepath: str| Path, verbose=False) -> bool:
     if not isinstance(content_dir_path, (str, Path)):
         print(f"content_dir_path must be a string or Path object.")
         return False
@@ -122,17 +143,21 @@ def generate_pages_recursive(content_dir_path: str | Path, template_path: str | 
         print(f"dest_dir_path must be a string or Path object.")
         return False
     dest_dir_path = Path(dest_dir_path)
+    if not isinstance(basepath, (str, Path)):
+        print(f"basepath must be a string or Path object.")
+        return False
+    basepath = Path(basepath)
     
     for dir_or_file in content_dir_path.iterdir():
         if dir_or_file.is_dir():
             new_dir_path = dest_dir_path / dir_or_file.name
             new_dir_path.mkdir()
             print(f"Created new directory: {new_dir_path}")
-            if not generate_pages_recursive(dir_or_file, template_path, new_dir_path, verbose):
+            if not generate_pages_recursive(dir_or_file, template_path, new_dir_path, basepath, verbose):
                 return False
         else:
             new_file_path = dest_dir_path / dir_or_file.name.replace("md", "html")
-            if not generate_page(dir_or_file, template_path, new_file_path, verbose):
+            if not generate_page(dir_or_file, template_path, new_file_path, basepath, verbose):
                 return False
     return True
 
